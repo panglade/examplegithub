8b04346dba56596c3d3ad6d7ad86d8ac54777384
Caridad Polanco <CaridadPolanco@Caridads-MacBook-Air.local>
2017-04-03 14:13:08 -0400

    Initial Commit

---
+++ Editing/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Editing
+//
+//  Created by Todd Perkins on 10/4/16.
+//  Copyright © 2016 Todd Perkins. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+var myProp = ""
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Editing/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Editing
+//
+//  Created by Todd Perkins on 10/4/16.
+//  Copyright © 2016 Todd Perkins. All rights reserved.
+//
+
+import Cocoa
+
+let e = ""  
+
+class ViewController: NSViewController {
+
+    var myProp = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+   
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
